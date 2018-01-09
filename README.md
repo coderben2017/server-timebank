d5a043200b7894e7b06342c1beb0d12f017ce70e
jinben <jinben@gagogroup.com>
2018-01-09 11:19:03 +0800

    chore(server/app&&build/app&&readme): use static source for server and update readme guide

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 the server for time bank web app
 
 ### Quick Start
+注：需要node.js环境
 ```
 npm install
 node build/app.js
+
+然后打开 http://localhost:3000 进入登录界面
 ```
--- build/app.js
+++ build/app.js
@@ -1,7 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
+var path = require("path");
 var bodyParser = require("body-parser");
+var connectHistoryApiFallback = require("connect-history-api-fallback");
 var mysql = require("mysql");
 var Plan_1 = require("./Plan");
 var User_1 = require("./User");
@@ -30,8 +32,8 @@ connection.connect();
  */
 var app = express();
 // 初始化配置
-// app.use('/', connectHistoryApiFallback()); // 使用浏览器路由
-// app.use('/', express.static(path.join(__dirname, '..', 'client'))); // 引用静态资源
+app.use('/', connectHistoryApiFallback()); // 使用浏览器路由
+app.use('/', express.static(path.join(__dirname, '..', 'client'))); // 引用静态资源
 app.use(bodyParser.urlencoded({ extended: false })); // post请求的body解析器（https://github.com/expressjs/body-parser）
 app.use(bodyParser.json());
 // login API
